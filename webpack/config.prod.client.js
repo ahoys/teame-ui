b37ab777c7ca184ddecc64f6590dffd6dee1c712
Ari Höysniemi <ari.hoysniemi@gmail.com>
2018-12-13 20:49:00 +0200

    Linted confs.

--- webpack/config.prod.client.js
+++ webpack/config.prod.client.js
@@ -1,48 +1,48 @@
-const path = require("path");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const StatsPlugin = require("stats-webpack-plugin");
-const root = path.resolve(__dirname, "../");
-const dist = path.resolve(__dirname, "../dist");
-const src = path.resolve(__dirname, "../src");
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const StatsPlugin = require('stats-webpack-plugin')
+const root = path.resolve(__dirname, '../')
+const dist = path.resolve(__dirname, '../dist')
+const src = path.resolve(__dirname, '../src')
 
 module.exports = {
-  mode: "production",
-  name: "client",
-  target: "web",
-  devtool: "nosources-source-map",
+  mode: 'production',
+  name: 'client',
+  target: 'web',
+  devtool: 'nosources-source-map',
   entry: `${src}/Client.tsx`,
   output: {
     path: dist,
     // Hash so that we can safely cache.
-    filename: "client.[chunkhash].js",
-    chunkFilename: "[name].[chunkhash].client.js"
+    filename: 'client.[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].client.js',
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx|js|jsx)$/,
-        use: "babel-loader",
-        exclude: /node_modules/
-      }
-    ]
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
     alias: {
       components: `${src}/components`,
-      dist
+      dist,
     },
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
-    modules: ["node_modules"]
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+    modules: ['node_modules'],
   },
   plugins: [
     // Cleans the destination folder before building new.
     new CleanWebpackPlugin([dist], { root }),
     // Creates stats for code splitting.
-    new StatsPlugin("stats.json")
+    new StatsPlugin('stats.json'),
   ],
   optimization: {
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
       maxInitialRequests: Infinity,
       minSize: 0,
       cacheGroups: {
@@ -52,11 +52,11 @@ module.exports = {
           name(module) {
             const packageName = module.context.match(
               /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-            )[1];
-            return `yarn.${packageName.replace("@", "")}`;
-          }
-        }
-      }
-    }
-  }
-};
+            )[1]
+            return `yarn.${packageName.replace('@', '')}`
+          },
+        },
+      },
+    },
+  },
+}
