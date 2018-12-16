e82bb16acb1a9c341ca5a62372edb906332fc041
Ari Höysniemi <ari.hoysniemi@gmail.com>
2018-12-16 23:57:05 +0200

    Added icons support.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,24 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 import React from 'react';
+import { FiHeart } from 'react-icons/fi';
 
 jsx;
 
 export default () => (
   <div
     css={{
-      background: 'red',
+      alignItems: 'center',
+      display: 'flex',
+      fontSize: '1rem',
+      height: '100vh',
+      justifyContent: 'center',
+      svg: {
+        marginLeft: '8px',
+      },
+      width: '100vw',
     }}
   >
-    Hello World!
+    Hello World! <FiHeart />
   </div>
 );
